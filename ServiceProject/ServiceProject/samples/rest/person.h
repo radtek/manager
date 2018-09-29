07d1293ea0f3975e628515068dfa13ea13245ecc
Alex <lordalex27@gmail.com>
2018-09-29 10:54:55 -0500

    service up 2

--- ServiceProject/ServiceProject/samples/rest/person.h
+++
@@ -1,6 +0,0 @@
-#import "custom/struct_tm_date.h"
-struct Person {
-  const char *name;
-  xsd__date dob;
-  enum Gender { MALE, FEMALE } gender;
-};
--- ServiceProject/ServiceProject/samples/wst/wstdemo.h
+++
@@ -1,4 +0,0 @@
-
-#import "wst.h" // use this for 2005/12 WS-Trust
-// #import "wst2.h" // use this for 2005/02 WS-Trust
-
